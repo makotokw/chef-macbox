8fd589736a494b400aa81dcc15bdd4a972696985
makoto_kw <makoto.kw@gmail.com>
2014-11-18 03:14:48 +0900

    Added tree and phpcs

--- site-cookbooks/arcadia/recipes/default.rb
+++ site-cookbooks/arcadia/recipes/default.rb
@@ -17,5 +17,7 @@
 # limitations under the License.
 #
 
+package 'tree'
+
 # ansible
 package 'ansible'
--- site-cookbooks/arcadia/recipes/php.rb
+++ site-cookbooks/arcadia/recipes/php.rb
@@ -4,3 +4,16 @@ template "#{node[:arcadia][:php][:conf_path]}/conf.d/arcadia.ini" do
   group 'admin'
   mode 00644
 end
+
+bash "install phpcs" do
+  cwd node[:user][:home]
+  group node[:user][:group]
+  code <<-EOH
+    composer global require 'squizlabs/php_codesniffer=*'
+    git clone https://github.com/WordPress-Coding-Standards/WordPress-Coding-Standards.git .wpcs
+    chown -R #{node[:user][:name]}:#{node[:user][:group]} #{node[:user][:home]}/.composer
+    chown -R #{node[:user][:name]}:#{node[:user][:group]} #{node[:user][:home]}/.wpcs
+    phpcs --config-set installed_paths #{node[:user][:home]}/.wpcs
+  EOH
+  environment 'HOME' => node[:user][:home]
+end
