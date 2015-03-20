4ea9ecd0916137c649d3bb7b33e2eb7f95cb1da4
makoto_kw <makoto.kw@gmail.com>
2015-03-20 18:21:12 +0900

    Install Symfony

--- site-cookbooks/arcadia/recipes/php.rb
+++ site-cookbooks/arcadia/recipes/php.rb
@@ -14,6 +14,20 @@ git "#{node[:arcadia][:project_path]}/contrib/WordPress-Coding-Standards" do
   action :sync
 end
 
+# Symfony
+ash "install symfony" do
+  cwd node[:user][:home]
+  user node[:user][:name]
+  group node[:user][:group]
+  code <<-EOH
+    curl -LsS http://symfony.com/installer > symfony.phar
+    sudo mv symfony.phar /usr/local/bin/symfony
+    chmod a+x /usr/local/bin/symfony
+  EOH
+  environment 'HOME' => node[:user][:home]
+end
+
+
 bash "install phpcs" do
   cwd node[:user][:home]
   user node[:user][:name]
