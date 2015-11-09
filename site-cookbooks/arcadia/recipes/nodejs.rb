d4bbc5710d0ce71c4e39608c332dbc1e51c3706a
makoto_kw <makoto.kw@gmail.com>
2015-11-10 01:21:46 +0900

    Fixed to install nodebrew at first

--- site-cookbooks/arcadia/recipes/nodejs.rb
+++ site-cookbooks/arcadia/recipes/nodejs.rb
@@ -1,5 +1,8 @@
 homebrew_package 'nodebrew'
 
-execute 'nodebrew install-binary stable'
+execute 'nodebrew setup'
+execute 'nodebrew install-binary stable' do
+  returns [0, 1]
+end
 execute 'nodebrew use stable'
-execute 'npm install -g gulp grunt-cli bower'
+execute 'export PATH=$HOME/.nodebrew/current/bin:$PATH;npm install -g gulp grunt-cli bower'
