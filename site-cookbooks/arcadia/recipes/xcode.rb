76d023470338a3fe8e4ce3642425031f12696619
makoto_kw <makoto.kw@gmail.com>
2018-04-09 22:03:44 +0900

    remove unuse code

--- site-cookbooks/arcadia/recipes/xcode.rb
+++ site-cookbooks/arcadia/recipes/xcode.rb
@@ -6,37 +6,3 @@ homebrew_package 're-install xcproj' do
   package_name 'xcproj'
   action :install
 end
-
-# plugin_path = "#{node[:arcadia][:project_path]}/contrib/XcodePlugins"
-# directory plugin_path do
-#   owner node[:user][:name]
-#   group node[:user][:group]
-#   mode 00755
-#   recursive true
-#   action :create
-# end
-#
-# # BBUncrustifyPlugin-Xcode
-# git "#{plugin_path}/BBUncrustifyPlugin-Xcode" do
-#   repository 'https://github.com/benoitsan/BBUncrustifyPlugin-Xcode.git'
-#   user node[:user][:name]
-#   group node[:user][:group]
-#   action :sync
-# end
-#
-# # Dash-Plugin-for-Xcode
-# git "#{plugin_path}/Dash-Plugin-for-Xcode" do
-#   repository 'https://github.com/omz/Dash-Plugin-for-Xcode.git'
-#   user node[:user][:name]
-#   group node[:user][:group]
-#   action :sync
-#   only_if { ::Dir.exist?('/Applications/Dash.app/') }
-# end
-#
-# # build by xcodebuild = install plugin
-# %w{BBUncrustifyPlugin-Xcode Dash-Plugin-for-Xcode}.each do |plugin|
-#   execute 'xcodebuild' do
-#     cwd "#{plugin_path}/#{plugin}"
-#     only_if { ::Dir.exist?("#{plugin_path}/#{plugin}") }
-#   end
-# end
